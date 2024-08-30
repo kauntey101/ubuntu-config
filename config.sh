c70897bf7213ba00b2122ae66833804df23abc8c
kauntey <kaunteya101@gmail.com>
2024-08-30 16:53:30 +0530

    Create config.sh - automation scripts

---
+++ config.sh
@@ -0,0 +1,8 @@
+sudo apt install tmux
+sudo apt install neovim
+sudo apt install batcat 
+# sudo apt install btop 
+sudo apt install net-tools
+clangd-18 # laest clangd language server
+sudo apt install net-tools # network tools
+sudo apt install make
