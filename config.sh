381d27eb0679a7bd07d24ef3d62b36c89477948f
kauntey <kaunteya101@gmail.com>
2024-08-31 22:55:45 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,13 @@
-sudo apt install tmux
-sudo apt install neovim
-sudo apt install batcat 
-# sudo apt install btop 
-sudo apt install net-tools
-clangd-18 # laest clangd language server
-sudo apt install net-tools # network tools
-sudo apt install make
+# INSALLATION OF TOOLS 
+# Language Servers
+sudo apt install clangd-18 -y 
+# Required tools for coding, debugging, LSP', syntax highlighting, code editors etc
+sudo apt install git make build-essential bat -y 
+# Networing Tools 
+sudo apt install curl wget nc net-tools -y
+# Web Developement related tools
+sudo apt install apache2 -y 
+# Terminal animation tools
+sudo apt install cmatrix cowsay -y
+# Others
+sudo apt install tmux zsh openssh-server -y 
