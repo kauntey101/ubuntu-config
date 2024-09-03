6e388fb05cf50d05e146ee7537ffe53eb415584b
kauntey <kaunteya101@gmail.com>
2024-09-03 10:37:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-# INSALLATION OF TOOLS 
+# Author - kauntey101
+# Note - The script requires root permissions to setup the machine
+
+# INSALLATION
 # Language Servers
 sudo apt install clangd-18 -y 
 # Required tools for coding, debugging, LSP', syntax highlighting, code editors etc
@@ -10,7 +13,9 @@ sudo apt install apache2 -y
 # Terminal animation tools
 sudo apt install cmatrix cowsay -y
 # Others
-sudo apt install tmux zsh openssh-server -y 
+sudo apt install tmux zsh openssh-server fzf nnn -y 
+# Programming Languages 
+sudo apt install python3 
 
 # CUSTOM CONFIGURATION
 echo 'export CPATH=~/libs/c++/' >> ~/.zshrc # Setting the CPATH environment variable for using custom c++ libraries easliy
