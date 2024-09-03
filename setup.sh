cf0bf9513aaefde9ff05159558d999cf20264af9
kauntey <kaunteya101@gmail.com>
2024-09-03 10:17:50 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# INSALLATION OF TOOLS 
+# Language Servers
+sudo apt install clangd-18 -y 
+# Required tools for coding, debugging, LSP', syntax highlighting, code editors etc
+sudo apt install git make build-essential bat -y 
+# Networing Tools 
+sudo apt install curl wget nc net-tools -y
+# Web Developement related tools
+sudo apt install apache2 -y 
+# Terminal animation tools
+sudo apt install cmatrix cowsay -y
+# Others
+sudo apt install tmux zsh openssh-server -y 
+
+# CUSTOM CONFIGURATION
+echo 'export CPATH=~/libs/c++/' >> ~/.zshrc # Setting the CPATH environment variable for using custom c++ libraries easliy
+echo "alias cat=\"batcat\"" >> ~/.zshrc # batcat instead of cat
+echo "alias clangd=\"clangd-18\"" >> ~/.zshrc # clangd-18 to clangd 
